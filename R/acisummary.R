89f28d469f57fdb596cae2d26cad78a649ceb26e
Joseph Ronald Stinziano <josephstinziano@gmail.com>
2019-10-02 12:33:16 -0600

    Add functions and documentation

---
+++ R/acisummary.R
@@ -0,0 +1,48 @@
+#' Extracts coefficients from fitacis2
+#'
+#' @param data data frame with A/Ci curves
+#' @param group1 grouping variable 1, must match fitacis2
+#' @param group2 grouping variable 2, must match fitacis2
+#' @param group3 grouping variable 3, must match fitacis2
+#' @param fits list output from fitacis2
+#'
+#' @return acisummary produces a data frame with A-Ci coefficients
+#' @importFrom tidyr unite
+#' @importFrom stats coef
+#' @export
+#'
+acisummary <- function(data,
+                       group1,
+                       group2,
+                       group3,
+                       fits){
+  data_output <- as.data.frame(1:length(fits))
+  colnames(data_output) <- "ID"
+  data$group1 <- data[,group1]
+  data$group2 <- data[,group2]
+  data$group3 <- data[,group3]
+  
+  data <- unite(data, col = "group",
+                from = c("group1", "group2", "group3"),
+                sep = "_")
+  
+  data <- split(data, data$group)
+  
+  for(i in 1:length(fits)){
+    data_output$ID[i] <- names(fits)[i]
+    data_output$Tleaf[i] <- mean(data[[i]]$Tleaf)
+    data_output$Patm[i] <- mean(fits[[i]]$df$Patm)
+    data_output$Vcmax[i] <- coef(fits[[i]])[1]
+    data_output$Jmax[i] <- coef(fits[[i]])[2]
+    data_output$TPU[i] <- coef(fits[[i]])[4]
+    data_output$Rd[i] <- coef(fits[[i]])[3]
+    data_output$Citrans1[i] <- fits[[i]]$Ci_transition
+    data_output$Citrans2[i] <- fits[[i]]$Ci_transition2
+    data_output$Km[i] <- fits[[i]]$Km
+    data_output$GammaStar[i] <- fits[[i]]$GammaStar
+    data_output$gmeso[i] <- fits[[i]]$gmeso
+  }
+  
+  return(data_output)
+  
+}
---
+++ R/fitacis2.R
@@ -0,0 +1,97 @@
+#' Fit A-Ci curves with custom kinetics
+#'
+#' @param data data frame with A/Ci curves
+#' @param group1 grouping variable 1, could be species, temperature, ID
+#' @param group2 grouping variable 2
+#' @param group3 grouping variable 3
+#' @param gm25 mesophyll conductance at 25C
+#' @param Egm activation energy of mesophyll conductance in kJ/mol
+#' @param K25 Km at 25C
+#' @param Ek activation energy of Km in kJ/mol
+#' @param fitmethod See ?fitaci in plantecophys
+#' @param fitTPU See ?fitaci in plantecophys. Set to TRUE/FALSE
+#' @param Tcorrect See ?fitaci in plantecophys. Default here is FALSE
+#' @param useRd See ?fitaci in plantecophys
+#' @param citransition See ?fitaci in plantecophys
+#' @param alphag See ?fitaci in plantecophys
+#' @param PPFD See ?fitaci in plantecophys
+#' @param Tleaf See ?fitaci in plantecophys
+#' @param alpha See ?fitaci in plantecophys
+#' @param theta See ?fitaci in plantecophys
+#' @param varnames See ?fitaci in plantecophys
+#'
+#' @return fitacis2 allows gmeso, GammaStar, and Km to vary with Tleaf
+#'         output matches the fitacis function from plantecophys
+#' @importFrom tidyr unite
+#' @importFrom plantecophys fitaci
+#' @export
+#'
+fitacis2 <- function(data,
+                     group1,
+                     group2,
+                     group3,
+                     gm25,
+                     Egm,
+                     K25,
+                     Ek,
+                     fitmethod,
+                     fitTPU,
+                     Tcorrect,
+                     useRd = FALSE,
+                     citransition = NULL,
+                     alphag = 0,
+                     PPFD = NULL,
+                     Tleaf = NULL,
+                     alpha = 0.24,
+                     theta = 0.85,
+                     varnames = list(ALEAF = "Photo", 
+                                     Tleaf = "Tleaf", 
+                                     Ci = "Ci",
+                                     PPFD = "PARi", 
+                                     Rd = "Rd")){
+  data$group1 <- data[,group1]
+  data$group2 <- data[,group2]
+  data$group3 <- data[,group3]
+  
+  data <- unite(data, col = "group",
+                from = c("group1", "group2", "group3"),
+                sep = "_")
+  
+  data <- split(data, data$group)
+  fits <- as.list(1:length(data))
+  
+  for (i in 1:length(data)){
+    gmeso <- gm25 * exp(Egm * 
+                          (mean(data[[i]]$Tleaf + 
+                                  273.15) - 298.15) /
+                          (298.15 * 
+                             mean(data[[i]]$Tleaf + 273.15) *
+                             0.008314))
+    
+    Km <- K25 * exp(Ek * 
+                      (mean(data[[i]]$Tleaf + 
+                              273.15) - 298.15) /
+                      (298.15 * 
+                         mean(data[[i]]$Tleaf + 273.15) *
+                         0.008314))
+    Patm <- mean(data[[i]]$Press)
+    
+    fits[[i]] <- fitaci(data[[i]], 
+                        Patm = Patm,
+                        varnames = varnames,
+                        fitmethod = fitmethod, 
+                        Tcorrect = Tcorrect,
+                        fitTPU = fitTPU,
+                        gmeso = gmeso, 
+                        Km = Km,
+                        useRd = useRd,
+                        citransition = citransition,
+                        alphag = alphag,
+                        PPFD = PPFD,
+                        Tleaf = Tleaf,
+                        alpha = alpha,
+                        theta = theta,)
+    names(fits)[i] <- data[[i]]$group[1]
+  }
+  return(fits)
+}
